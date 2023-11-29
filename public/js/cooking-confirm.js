cbdcfd4d5522ee717552f1304172343ce3240987
syonosuke <syonosuke1996@outlook.jp>
2023-11-29 23:05:24 +0900

    選択された項目を削除に挿入する処理

--- public/js/cooking-confirm.js
+++ public/js/cooking-confirm.js
@@ -1,20 +1,26 @@
-//料理名を追加する関数
-window.onload = function (){
-  const recipeName = document.getElementById('recipe-name');
-  let text = '<li>追加テキスト</li>';
-  recipeName.insertAdjacentHTML('afterbegin',text);
-}
-
-//材料をチェックボックス付きのテキストで追加する関数
-window.onload = function(){
-  const materialName = document.getElementById('material-name');
-  let text = '<div><input type="checkbox" id="check" name="check" >追加テキスト<div>';
-  materialName.insertAdjacentHTML('afterbegin',text);
-}
-
-function mycheck() {
-  let flag = false;//チェックされているかを判定する変数
-  (let i = 0; i < document.form.check.lenth; i++){
-    
+function handleRespons(response){
+  if(!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`)
   }
+  return response.json;
 }
+
+//料理名にリストで挿入する
+window.onload = function cook(){
+  const recipe = document.getElementById("recipe-name");
+  recipe.insertAdjacentHTML("afterbegin","<li>料理名</li>");
+}
+
+//材料一覧にチェックボックス付きでデータを挿入。チェックボックスが選択されたら削除した材料に挿入
+window.onload = function matelial(){
+  const material = document.getElementById("material-name");
+  material.insertAdjacentHTML
+  ("afterbegin","<div><input type ='checkbox' id=material-check >材料<input type = 'text' id = amount size ='1'><span>個</span></div>");
+  material-check.addEventListener('change', function(){
+    const deleteMaterial = document.getElementById("delete-material");
+    deleteMaterial.insertAdjacentHTML
+    ("afterbegin","<li>チェックされた材料</li>");
+  });
+}
+
+
