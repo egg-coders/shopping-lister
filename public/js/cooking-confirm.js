13b2352bcd371486080a734aec05fca2f544beb4
syonosuke <syonosuke1996@outlook.jp>
2023-11-30 20:01:42 +0900

    ページ変遷の処理とJsの編集

--- public/js/cooking-confirm.js
+++ public/js/cooking-confirm.js
@@ -4,6 +4,9 @@ function handleResponse(response){
   }
   return response.json;
 }
+//ページ３から
+const idListStr = sessionStorage.getItem("selectedIdList");
+const idList = JSON.parse(idListStr);
 
 //料理名にリストで挿入する
 window.onload = function cook(){
@@ -15,12 +18,14 @@ window.onload = function cook(){
 window.onload = function matelial(){
   const material = document.getElementById("material-name");
   material.insertAdjacentHTML
-  ("afterbegin","<div><input type ='checkbox' id='material-check' >材料<input type = 'text' id = 'amount' size ='1'><span>個</span></div>");
+  ("afterbegin","<div id = 'mate'><input type ='checkbox' id='material-check' >材料<input type = 'text' id = 'amount' size ='1'><span id= 'unit' >個</span></div>");
   const materialCheck = document.getElementById("material-check")
   materialCheck.addEventListener('change', function(){
     const deleteMaterial = document.getElementById("delete-material");
     deleteMaterial.insertAdjacentHTML
-    ("afterbegin","<li>チェックされた材料</li>");
+    ("afterbegin","<div id = 'mate'><input type ='checkbox' id='material-check' >材料<input type = 'text' id = 'amount' size ='1'><span id= 'unit' >個</span></div>");
+    const listElement = document.getElementById('mate');
+    listElement.remove();
   });
 }
 
